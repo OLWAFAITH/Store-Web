1d3e7f6503c7b3f96ac10e3e2439333a9d9fb0a8
OLUWAFAITH <alukofaithtemitope7@gmail.com>
2021-10-23 19:14:52 -0700

    store web

---
+++ adewale.js
@@ -0,0 +1,8 @@
+function available(){
+    homepage.style.display = 'none';
+    body.style.display='block';
+ }
+ function art(){
+    body.style.display = 'none';
+    milopackage.style.display = 'block';
+ }
